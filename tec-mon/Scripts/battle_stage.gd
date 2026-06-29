7ed636b79449f97723ffaf1ea41abad08c3a2961
Tony Nadeau <tonynadeau03@gmail.com>
2026-06-28 22:50:13 -0400

    Fixed battle bug where inputting a move during another move caused game to break

--- tec-mon/Scripts/battle_stage.gd
+++ tec-mon/Scripts/battle_stage.gd
@@ -16,12 +16,17 @@ extends CanvasLayer
 @onready var move_three: Button = %MoveThree
 @onready var move_four: Button = %MoveFour
 
+var can_input: bool = false
+var buttons: Array[Button]
+
 func _ready() -> void:
 	EncounterManager.encounter_started.connect(_on_encounter_started)
 	BattleSystem.battle_started.connect(_on_battle_started)
 	BattleSystem.battle_ended.connect(_on_battle_ended)
 	BattleSystem.turn_ended.connect(_on_turn_ended)
 	BattleSystem.move_executed.connect(_on_move_executed)
+	
+	buttons = [move_one, move_two, move_three, move_four]
 	
 	move_one.pressed.connect(_on_move_button_pressed.bind(0))
 	move_two.pressed.connect(_on_move_button_pressed.bind(1))
@@ -37,6 +42,7 @@ func _on_encounter_started(enemy_instance: TecmonInstance) -> void:
 	await MessageBus.message_box_closed
 
 	await SceneManager._transition_out()
+	AudioManager.play_music(preload("res://Assets/Sounds/Music/battle_theme.wav"))
 	var party: Array[TecmonInstance] = [
 		TecmonInstance.create(
 			get_tree().get_first_node_in_group("Player").starter_tecmon, 4, false
@@ -52,12 +58,19 @@ func _on_battle_started() -> void:
 	_refresh_hp_bars()
 	new_turn()
 
+func _set_battle_buttons_enabled(enabled: bool) -> void:
+	can_input = enabled
+
+	for button in buttons:
+		button.disabled = not enabled
+
 func _on_turn_ended() -> void:
 	## HP bars were already updated after each move_executed signal.
 	## Just show the action prompt and menu again.
 	new_turn()
 
 func new_turn() -> void:
+	_set_battle_buttons_enabled(true)
 	move_container.hide()
 	MessageBus.send_passive(
 		"What will " + BattleSystem.player_participant.display_name() + " do?"
@@ -88,7 +101,11 @@ func _on_move_executed(_user: BattleParticipant, _target: BattleParticipant,
 #Action menu buttons
 
 func _on_fight_pressed() -> void:
+	if not can_input:
+		return
+	
 	move_container.show()
+	AudioManager.play_sfx("select")
 	MessageBus._message_box._clear_passive()
 	var inst: TecmonInstance = BattleSystem.player_participant.instance
 	var buttons := [move_one, move_two, move_three, move_four]
@@ -106,16 +123,31 @@ func _on_move_button_pressed(index: int) -> void:
 	var move: MoveInstance = inst.moves.get(index)
 	if move == null:
 		return
+		
+	_set_battle_buttons_enabled(false)
 	move_container.hide()
+	AudioManager.play_sfx("select")
 	BattleSystem.queue_move(move)
 
 func _on_items_pressed() -> void:
+	if not can_input:
+		return
+		
+	AudioManager.play_sfx("select")
 	pass  ## TODO
 
 func _on_tecmons_pressed() -> void:
+	if not can_input:
+		return
+		
+	AudioManager.play_sfx("select")
 	pass  ## TODO
 
 func _on_escape_pressed() -> void:
+	if not can_input:
+		return
+		
+	AudioManager.play_sfx("select")
 	BattleSystem.queue_flee()
 
 func _on_battle_ended(outcome: BattleSystem.BattleOutcome) -> void:
@@ -132,4 +164,5 @@ func _on_battle_ended(outcome: BattleSystem.BattleOutcome) -> void:
 	MessageBus._message_box.switch_mode()
 	await SceneManager._transition_out()
 	hide()
+	AudioManager.play_music(SceneManager.current_level.bgm)
 	await SceneManager._transition_in()
