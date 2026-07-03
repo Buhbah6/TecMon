7018edae3c0913461af479b49f908b65384547a6
Tony Nadeau <tonynadeau03@gmail.com>
2026-07-03 14:27:02 -0400

    Fixed Inventory Bug

--- tec-mon/Scripts/UI/bag_ui.gd
+++ tec-mon/Scripts/UI/bag_ui.gd
@@ -6,4 +6,8 @@ func _ready() -> void:
 func _on_back_button_pressed() -> void:
 	get_tree().paused = false
 	hide()
-	
+
+func _on_visibility_changed() -> void:
+	if visible:
+		get_node("%TecMon")._populate()
+		get_node("%Items")._populate()
